7b20e55fb7fc8b6bf1f8f4026bf315d015ecafcd
Jonathan Odlum <jonathan.odlum@gmail.com>
2018-09-24 20:17:51 +1000

    Basic game rules inplemented

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
 console.log('Working!');
+let test;
 
-
-let gridSize = 3;
 $(document).ready(function(){
-  let test = new Board(gridSize);
+  test = new Board(gridSize);
 });
 
 // Global Variables:
+  const gridSize = 3;
+  let playerTurn = 'X';
 
+  claims = {
+    claimedO: {
+      x0: 0,
+      x1: 0,
+      x2: 0,
+      y0: 0,
+      y1: 0,
+      y2: 0,
+      d1: 0,
+      d2: 0,
+    },
+    claimedX: {
+      x0: 0,
+      x1: 0,
+      x2: 0,
+      y0: 0,
+      y1: 0,
+      y2: 0,
+      d1: 0,
+      d2: 0,
+    },
+  }
 // Global Variables end.
 
 
@@ -24,7 +47,7 @@ const Board = function(size){
     width: 600,
     height: 600,
     position: 'relative',
-    top: '100px',
+    top: '50px',
     left: '100px',
   });
   $('#gameContainer').append(this.div)
@@ -41,9 +64,10 @@ const Board = function(size){
 const Cell = function(x,y,parent){
   this.coOrdX = x;
   this.coOrdY = y;
+  this.winner = null;
   this.offsetX = x*200;
   this.offsetY = y*200;
-  this.div = $('<div class="cell">');
+  this.div = $(`<div class="cell" id='${x}${y}'>`);
   this.div.on('click', function(){
     gameLogic(this);
   })
@@ -58,4 +82,47 @@ const Cell = function(x,y,parent){
     left: `${this.offsetY}px`,
     'background-color': '#FF0000',
   });
+}
+
+Cell.prototype.clicked = function(){
+
+}
+
+const trackClaims = function(player, x, y){
+  const playersClaims = claims[`claimed${player}`];
+  playersClaims[`x${x}`] ++;
+  playersClaims[`y${y}`] ++;
+  if( x+y%2 ===0 ){
+    if( x===y ){
+      playersClaims['d1'];
+    }
+    if(x+y === 2)
+    {
+      playersClaims['d2'];
+    }
+  }
+}
+
+const checkForWin = function(player){
+  for (let key in claims[`claimed${player}`]){
+    console.log("checking");
+    if (claims[`claimed${player}`][key] === 3){
+      alert(`${player} WINS!`);
+    }
+  }
+}
+
+const gameLogic = function(clickedCell){
+  $(clickedCell).html(playerTurn);
+  $(clickedCell).off('click');
+  cellId = $(clickedCell).attr('id');
+  test.cells[cellId[0]][cellId[1]].winner = playerTurn;
+  trackClaims(playerTurn, cellId[0], cellId[1]);
+  checkForWin(playerTurn);
+
+  if (playerTurn === 'X'){
+    playerTurn = 'O';
+  }else if (playerTurn === 'O'){
+    playerTurn = 'X';
+  }
 }
