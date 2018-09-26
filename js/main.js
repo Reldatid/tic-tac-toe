6ab64811c8d8b5ccf0e44d9053e8c53a5f3775b0
Jonathan Odlum <jonathan.odlum@gmail.com>
2018-09-26 22:47:43 +1000

    pre branch implementation. back to where we left off, but better.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 console.log('hoy');
-const layers = 2;
-const activeBranch = '';
-const clickables = [];
-let tree = {};
+const layers = 3;
+const activeBranch = '0';
+const cellObjs = [];
+let treeTop;
+let playerTurn = 'O';
+let winner = 'none';
+let gameOver = false;
 
 const makeBoard = function(){
-  makeCellsFor(tree, '0', layers);
+  treeTop = new Cell('none', 0, '00', layers)
+  makeCellsFor(treeTop, '0', layers);
   let $container = $('#gameContainer');
-  $container.append(tree.$grid);
+  $container.append(treeTop.$grid);
   tree.$grid.append($('<img src="images/tictactoeBoard.png" />'));
 }
 
@@ -44,6 +48,7 @@ const Cell = function(parent, branch, coOrdinates, layer){
     this.$grid.append($image);
     this.$div.append(this.$grid);
     this.cells = {};
+    this.numberOfClaims = 0;
     this.playerClaims = {
       claimedO: {
         x0: 0,
@@ -68,15 +73,107 @@ const Cell = function(parent, branch, coOrdinates, layer){
     };
 
   }else{
-    this.$div.attr('id', `${clickables.length}`);
+    this.$div.attr('id', `${cellObjs.length}`);
     this.$div.on('click', function(){
-
+      let id = $(this).attr('id');
+      gameLoop(id);
     })
-    clickables.push(this);
+    cellObjs.push(this);
   }
-  this.parent.$grid.append(this.$div);
+  if(this.parent !== 'none'){
+    this.parent.$grid.append(this.$div);
+  }
 }
 
+Cell.prototype.wasClicked = function(){
+  if (this.claimer){
+    console.log("This cell has already been claimed.");
+  }else{
+    this.claimThis(playerTurn);
+  }
+}
+
+Cell.prototype.claimThis = function(claimer){
+  this.claimer = claimer;
+  if(this.parent !== 'none'){
+    this.parent.trackClaims(this.coOrds, claimer);
+    this.parent.checkForWin(this.coOrds);
+    this.claimCascade();
+    this.shiftBranch(this.branch);
+  } else {
+    winner = claimer;
+    gameOver = true;
+  }
+  this.updateImage();
+};
+
+Cell.prototype.claimCascade = function () {
+  if(this.layer !== 0){
+    for (let key in this.cells){
+      this.cells[key].claimer = 'O';
+      this.cells[key].claimCascade();
+    }
+  }
+};
+
+Cell.prototype.checkForWin = function(coOrds){
+  const x = parseInt(coOrds[0]);
+  const y = parseInt(coOrds[1]);
+  const claims = this.playerClaims[`claimed${playerTurn}`];
+  if (this.numberOfClaims < 9){
+    if ( (claims[`x${x}`] === 3) || (claims[`y${y}`] === 3) || (claims.d1 === 3) || (claims.d2 === 3) ){
+      this.claimThis(playerTurn);
+      return;
+    }
+  }else{
+    this.claimThis('O');
+    this.claimThis('X');
+    this.claimer = 'both';
+  }
+};
+
+Cell.prototype.trackClaims = function(coOrds, claimer){
+  claims = this.playerClaims[`claimed${claimer}`];
+  const x = parseInt(coOrds[0]);
+  const y = parseInt(coOrds[1]);
+  claims[`x${x}`] ++;
+  claims[`y${y}`] ++;
+  if( (x+y)%2 === 0 ){
+    if( x === y ){
+      claims.d1 ++;
+    }
+    if(x+y === 2)
+    {
+      claims.d2 ++;
+    }
+  }
+};
+
+Cell.prototype.shiftBranch = function(){
+  const depth = layers - this.layer;
+
+};
+
+Cell.prototype.updateImage = function(){
+  if (this.claimer === 'both'){
+    $image = $(`<img src="images/O.png" />`);
+    this.$div.append($image);
+    $image = $(`<img src="images/X.png" />`);
+    this.$div.append($image);
+    return;
+  }
+  $image = $(`<img src="images/${this.claimer}.png" />`);
+  this.$div.append($image);
+};
+
+const gameLoop = function(clickedCellid){
+  if(!gameOver){
+    cellObj = cellObjs[clickedCellid];
+    cellObj.wasClicked();
+  }else {
+    //Suggest Reset. Or just alert an obnoxious message.
+  }
+}
 
 tree = {
   cells: {},
